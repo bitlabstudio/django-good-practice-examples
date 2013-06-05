b1e8bd617a0234ad7b6c8e11f9cf0ad0caa5a2e8
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-06-05 15:13:03 +0200

    Added goals, sectors and countries to the view context

--- good_practice_examples/urls.py
+++ good_practice_examples/urls.py
@@ -1,16 +1,12 @@
 """URLs for the ``good_practice_examples`` app."""
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import ListView
-from django.http import HttpResponse
 
-from .models import GoodPracticeExample
+from .views import GoodPracticeExampleListView
 
 
 urlpatterns = patterns(
     '',
     url(r'$',
-        ListView.as_view(
-            model=GoodPracticeExample,
-            template_name='good_practice_examples/examples_list.html'),
+        GoodPracticeExampleListView.as_view(),
         name='good_practice_example_list'),
 )
--- good_practice_examples/views.py
+++ good_practice_examples/views.py
@@ -1,3 +1,26 @@
 """Views for the ``good_practice_examples`` app."""
+from django.views.generic import ListView
 
-# they've been directly hooked up to the urls
+from .models import GoodPracticeExample
+
+
+class GoodPracticeExampleListView(ListView):
+    """View to list ``GoodPracticeExample`` objects."""
+    model = GoodPracticeExample
+    template_name = 'good_practice_examples/examples_list.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(GoodPracticeExampleListView, self).get_context_data(
+            **kwargs)
+        countries = []
+        goals = []
+        sectors = []
+        for example in self.object_list:
+            countries.extend(example.countries.all())
+            goals.extend(example.goals.all())
+            sectors.extend(example.sectors.all())
+        ctx.update({
+            'goals': list(set(goals)),
+            'sectors': list(set(sectors)),
+            'countries': list(set(countries))})
+        return ctx
